975c8d3a1d1033b22fbef76644767c4650961541
oleg <iq2017@yandex.ru>
2025-10-19 17:23:20 +0300

    добавил вывод времени

--- test.go
+++ test.go
@@ -3,10 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 )
 
 func main() {
 	name := flag.String("name", "World", "Имя для приветствия")
 	flag.Parse()
 	fmt.Printf("Hello, %s! from first_app\n", *name)
+
+	// Вывод текущего времени
+	fmt.Println("Current time:", time.Now())
 }
